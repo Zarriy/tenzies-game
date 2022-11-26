68deb06978ce1448a20cf0608fafa63da3b50061
zarriy <zarriy@outlook.com>
2022-11-26 09:22:45 +0500

    build the basic layout of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import { useState } from "react";
+import Die from "./die";
 import "./App.css";
 
 function App() {
-  return <div>This works</div>;
+  const [dice, setDice] = useState(createDice());
+  function randomValue() {
+    return Math.ceil(Math.random() * 6);
+  }
+  function createDice() {
+    const array = [];
+    for (let x = 0; x < 10; x++) {
+      const die = {
+        value: randomValue(),
+        hold: false,
+        id: x + 1,
+      };
+      array.push(die);
+    }
+    return array;
+  }
+
+  return (
+    <div className="game-div">
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="Dice-Box">
+        {dice.map((die) => (
+          <Die dice={die.value} key={die.id} />
+        ))}
+        <button className="roll-dice">Roll</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/die.jsx
@@ -0,0 +1,8 @@
+function Die(props) {
+  return (
+    <div className="dice-div">
+      <h2>{props.dice}</h2>
+    </div>
+  );
+}
+export default Die;
